7a3d6473098a9dcbbd5059542da9f49687635bd2
KarurRenga <127001390+KarurRenga@users.noreply.github.com>
2023-03-05 09:18:47 +0530

    Add files via upload

---
+++ week1/day1/AssignPrimenumber.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class AssignPrimenumber {
+public static void main(String[] args) {
+	int number=44;
+	int check=0;
+		for (int i=2; i<=number/2 ; i++) {
+			if (number%i==0)
+			{
+				System.out.println("The given number "+ number + " is non prime" );
+				check=1;
+				break;
+			}
+					
+	}
+		if(check==0)
+		{
+			System.out.println("The given number "+ number + " is prime" );
+		}
+		
+		}
+}
