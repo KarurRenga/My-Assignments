bf28f1abe8313a4392c10510757464b9069c43a6
KarurRenga <127001390+KarurRenga@users.noreply.github.com>
2023-03-12 10:52:14 +0530

    Add files via upload

---
+++ Week2/Day1/Assignment2.java
@@ -0,0 +1,64 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.edge.EdgeDriver;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+	
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		
+		ChromeDriver driver = new ChromeDriver(option);
+//		EdgeDriver driver = new EdgeDriver();
+
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		Thread.sleep(3000);
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		
+//		driver.findElement(By.name("USERNAME")).sendKeys("DemoCSR");
+//		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+//		driver.findElement(By.className("loginButton")).click();
+		
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		
+			
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("RENGA & CO");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Rengarajan");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("VASUDEVAN");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Renga");
+		driver.findElement(By.name("departmentName")).sendKeys("Testing");
+		driver.findElement(By.name("description")).sendKeys("Renga creating scripts.Thanks to Testleaf");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("emailofrenga@gmail.com");
+		WebElement stateProv= driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select state = new Select(stateProv);
+		state.selectByVisibleText("New York");
+		
+		driver.findElement(By.name("submitButton")).click();
+		
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.name("description")).clear();
+		driver.findElement(By.name("importantNote")).sendKeys("Excellent work");
+		driver.findElement(By.name("submitButton")).click();
+			
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+	//	driver.close();
+	}
+
+}
