79a1e5c1f08bc8115e834393275cd770508d994e
KarurRenga <127001390+KarurRenga@users.noreply.github.com>
2023-06-16 15:49:21 +0530

    Add files via upload

---
+++ RestApi/Week2/Assignment/Jira/W2Assg-1.bat
@@ -0,0 +1,4 @@
+
+cd C:\Test Leaf\REST API\WEEK2\weekly assigments
+
+newman run "W2D1-Chaining of requests in JIRA Copy.postman_collection.json" -e Jira.postman_environment.json -g workspace.postman_globals.json -r htmlextra
